6bb237ee3e93307d6c4ef7e78b73a3319a63a3ed
Hwarang <keyein123@gmail.com>
2024-03-28 14:29:19 +0900

    commit

--- Alg_Studying/DFS_BFS/B1149_달이차오른다가자_오화랑(Bit).java
+++ Alg_Studying/DFS_BFS/B1149_달이차오른다가자_오화랑(Bit).java
@@ -5,101 +5,99 @@ import java.util.*;
 public class B1149_달이차오른다가자_오화랑 {
     static class State {
         int x, y, spend, key;
-        boolean made;
         public State(int x, int y, int spend, int key) {
             this.x = x;
             this.y = y;
             this.spend = spend;
             this.key = key;
-            this.made = false;
-        }
-        public State(int x, int y, int spend, int key, boolean made) {
-            this.x = x;
-            this.y = y;
-            this.spend = spend;
-            this.key = key;
-            this.made = made;
-        }
-        @Override
-        public String toString() {
-            return "State{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    ", spend=" + spend +
-                    ", key=" + key +
-                    ", made=" + made +
-                    '}';
         }
     }
     static int[][] Map;
+    static boolean[][] visited;
+    static int[][] keyVisited;
+    static int N, M;
+    static int minTime = Integer.MAX_VALUE;
     static boolean canGo;
-    static int N, M, minTime;
     static Queue<State> queue = new ArrayDeque<>();
+    static int[][] move = { {1, 0}, {0, -1}, {-1, 0}, {0, 1} };
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(input.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         Map = new int[N][M];
-
+        visited = new boolean[N][M];
+        keyVisited = new int[N][M];
         String eachR;
         int startX, startY;
         startX = startY = 0;
-        for (int i = 0 ; i < N ; i++) {
+        for (int i = 0; i < N; i++) {
             eachR = input.readLine();
-            for (int j = 0 ; j < M ; j++) {
+            for (int j = 0; j < M; j++) {
                 switch (eachR.charAt(j)) {
                     // 입구(1개) & 출구(1개 이상)
                     case '0' : { Map[i][j] = 100; startX = i; startY = j; break; }
                     case '1' : Map[i][j] = 200; break;
                     case '.' : Map[i][j] = -100; break; case '#' : Map[i][j] = -200; break;
-
                     case 'a' : Map[i][j] = 1; break; case 'b' : Map[i][j] = 2; break;
                     case 'c' : Map[i][j] = 3; break; case 'd' : Map[i][j] = 4; break;
                     case 'e' : Map[i][j] = 5; break; case 'f' : Map[i][j] = 6; break;
-
                     case 'A' : Map[i][j] = -1; break; case 'B' : Map[i][j] = -2; break;
                     case 'C' : Map[i][j] = -3; break; case 'D' : Map[i][j] = -4; break;
                     case 'E' : Map[i][j] = -5; break; case 'F' : Map[i][j] = -6; break;
                 }
             }
         }
-        int[][] move = { {1, 0}, {0, -1}, {-1, 0}, {0, 1} };
-        queue.offer(new State(startX, startY,0, 0));
-        while (!queue.isEmpty()) {
-            State tempState = queue.poll();
-            System.out.println(tempState);
-            int currX = tempState.x;
-            int currY = tempState.y;
-            int currT = tempState.spend;
+        queue.offer(new State(startX, startY, 0,0));
+        visited[startX][startY] = true;
+        keyVisited[startX][startY] = 0;
 
-            if (tempState.made) {
-                minTime = tempState.spend;
+        State tempState;
+        int currX, currY, currT, currKey;
+        int nextX, nextY;
+        while (!queue.isEmpty()) {
+            tempState = queue.poll();
+            currX = tempState.x;
+            currY = tempState.y;
+            currT = tempState.spend;
+
+            if (Map[currX][currY] == 200) {
+                minTime = currT;
                 canGo = true;
                 break;
             }
 
-            int nextX, nextY, currKey;
             for (int[] eachM : move) {
+                currKey = tempState.key;
                 nextX = currX + eachM[0];
                 nextY = currY + eachM[1];
-                currKey = tempState.key;
-                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue; // 바깥으로 이동 X
-                if (Map[nextX][nextY] == -200) continue; // 벽은 도달 X
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+                if (Map[nextX][nextY] == -200) continue;
+                if (visited[nextX][nextY] && (~keyVisited[nextX][nextY] & currKey) == 0) continue;
 
                 if (Map[nextX][nextY] < 0) { // 일반 구역 혹은 Door 구역 Case
                     if (Map[nextX][nextY] == -100) { // 일반 구역은 제한 없이 도달 O
                         queue.offer(new State(nextX, nextY,currT + 1, currKey));
+                        visited[nextX][nextY] = true;
+                        keyVisited[nextX][nextY] = currKey;
                     }
                     else {
-                        if ((currKey & (1 << (-Map[nextX][nextY] - 1))) != 0)
+                        if ((currKey & (1 << (-Map[nextX][nextY] - 1))) != 0) {
                             queue.offer(new State(nextX, nextY, currT + 1, currKey));
+                            visited[nextX][nextY] = true;
+                            keyVisited[nextX][nextY] = currKey;
+                        }
                     }
                 }
                 else {
-                    if (Map[nextX][nextY] == 200) tempState.made = true;
-                    else if (Map[nextX][nextY] != 100) currKey |= 1 << (Map[nextX][nextY] - 1);
-                    queue.offer(new State(nextX, nextY, currT + 1, currKey, tempState.made));
+                    if (Map[nextX][nextY] < 100) {
+                        if ((currKey & (1 << (Map[nextX][nextY] - 1))) == 0) {
+                            currKey |= 1 << (Map[nextX][nextY] - 1);
+                        }
+                    }
+                    queue.offer(new State(nextX, nextY, currT + 1, currKey));
+                    visited[nextX][nextY] = true;
+                    keyVisited[nextX][nextY] = currKey;
                 }
             }
         }
@@ -107,6 +105,8 @@ public class B1149_달이차오른다가자_오화랑 {
         else System.out.println(-1);
     }
 }
+
+
 // 0 -> 100, 1 -> 200
 // . -> -100, # -> -200
 // Key 상태 = "000000"
