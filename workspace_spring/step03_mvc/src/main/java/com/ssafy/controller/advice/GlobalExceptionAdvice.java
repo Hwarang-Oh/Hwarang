c41b31ebc09c4091cb6f5c1512b21eb159e53fc0
hwarang <keyein123@gmail.com>
2024-04-19 18:02:07 +0900

    git commit

--- workspace_spring/step03_mvc/src/main/java/com/ssafy/controller/advice/GlobalExceptionAdvice.java
+++ workspace_spring/step03_mvc/src/main/java/com/ssafy/controller/advice/GlobalExceptionAdvice.java
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice // AOP의 특성을 담고 있음
 public class GlobalExceptionAdvice {
 
-    @ExceptionHandler(Exception.class) // 마치 AOP의 After Throwing 처럼 예외를 Catch하는 효과가 있다. (smth like catch in try ~ catch
-                                       // )
+    // (smth like catch in try ~ catch)
+    @ExceptionHandler(Exception.class) // 마치 AOP의 After Throwing 처럼 예외를 Catch하는 효과가 있다.
     public String handleException(Exception e, Model model) {
         e.printStackTrace();
         model.addAttribute("exceptionMsg", e.getMessage());
         return "error"; // Error Page로 forwarding 하면 됨
     }
-} // 이걸 Dept Controller 안에 넣게 되면 => Dept Controller만의 예외 처리가 되는 것이고, 나머지는 여기로 오게 될
-  // 것이다!
+}
+// 이걸 Dept Controller 안에 넣게 되면 => Dept Controller만의 예외 처리가 되는 것이고, 나머지는 여기로 오게 될
+// 것이다!
