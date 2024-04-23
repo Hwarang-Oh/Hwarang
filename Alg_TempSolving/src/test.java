1d49ffd314fe49a1ccde56cd0e0433a025ce35f5
Hwarang <keyein123@gmail.com>
2024-04-23 23:56:07 +0900

    240423_last_commit

--- Alg_TempSolving/src/test.java
+++ Alg_TempSolving/src/test.java
@@ -1,11 +1,19 @@
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class test {
-    public static void main(String[] args) {
-        BigInteger x = new BigInteger("21231231231");
-        BigInteger y = x.pow(123123123);
-        BigInteger z = y.mod(new BigInteger("1000"));
-        System.out.println(z);
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = input.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i)))
+                sb.append(Character.toLowerCase(str.charAt(i)));
+            else
+                sb.append(Character.toUpperCase(str.charAt(i)));
+        }
+        System.out.println(sb);
     }
 
 }
