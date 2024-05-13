2bdbb40fc4b296b3b54365c9d81260781f73d3b7
Hwarang <keyein123@gmail.com>
2024-05-14 06:48:03 +0900

    240514_Morning

--- Alg_TempSolving/src/B2448_별찍기11_오화랑.java
+++ Alg_TempSolving/src/B2448_별찍기11_오화랑.java
@@ -1,63 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B2448_별찍기11_오화랑 {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(input.readLine());
-        // drawStar(size, 0);
-        System.out.print(sb);
-        // drawStart(size, size / 2, size / 2 + size);
-    }
 
-    public static void drawStar(int size, int startPoint1, int startPoint2) {
-        if (size > 3) {
-
-        }
-
-        for (int i = size - 2; i <= size; i++) {
-            if (i == size) {
-                for (int t = 1; t < startPoint; t++)
-                    sb.append(" ");
-                for (int t = 1; t < size * 2; t++) {
-                    if (t % 6 == 0)
-                        sb.append(" ");
-                    else
-                        sb.append("*");
-                }
-            } else if (i == size - 1) {
-                for (int t = 1; t < startPoint; t++)
-                    sb.append(" ");
-                for (int t = 1; t < size * 2; t++) {
-                    if (t % 2 == 1 || t % 6 == 0)
-                        sb.append(" ");
-                    else
-                        sb.append("*");
-                }
-            } else {
-                for (int t = 1; t < startPoint; t++)
-                    sb.append(" ");
-                for (int t = 1; t < size * 2; t++) {
-                    if (t % 3 == 0 && t % 6 != 0)
-                        sb.append("*");
-                    else
-                        sb.append(" ");
-                }
-            }
-
-            sb.append("\n");
-        }
     }
 }
 
 // 규칙을 유추한 뒤에 별을 찍자
 // N은 항상 3 * 2^k (k : 0 ~ 10) => 3, 6, 12, 24, 48, 96, 192, 384, 768, 1536, 3072
 /*
- * 6
- * * 5 (6) 7
- ***** 4 5 6 7 8
- * * 3 (4 5 6 7 8) 9
- * * * *
- ***** *****
+ * 3 : 2배 => 2개
+ * 6 : 동결 => 2개
+ * 9 : 2배 => 4개
+ * 
+ * 12 : 2개
+ * 15 : 2배 -> 4개
+ * 18 : 동결 -> 4개
+ * 21 : 2배 -> 8개
+ * 
+ * 24 : 2개
+ * 27 : 2배 -> 4개
+ * 30 : 동결 -> 4개
+ * 33 : 2배 -> 8개
+ * 36 :
  */
--- Java_Project/ALG_HappyHome/src/main/webapp/assets/js/map.js
+++ Java_Project/ALG_HappyHome/src/main/webapp/assets/js/map.js
@@ -1,8 +1,8 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-	mapOption = {
-		center: new kakao.maps.LatLng(37.56813, 127.00213), // 지도의 중심좌표
-		level: 5, // 지도의 확대 레벨
-	};
+  mapOption = {
+    center: new kakao.maps.LatLng(37.56813, 127.00213), // 지도의 중심좌표
+    level: 5, // 지도의 확대 레벨
+  };
 
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
@@ -25,171 +25,182 @@ map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 var geocoder = new kakao.maps.services.Geocoder();
 var currentCodes = new Set();
 
-kakao.maps.event.addListener(map, 'bounds_changed', function() {
-	if (isMoving) {
-		return;
-	}
-	drawApts();
+kakao.maps.event.addListener(map, 'bounds_changed', function () {
+  if (isMoving) {
+    return;
+  }
+  drawApts();
 });
 
 function drawApts() {
-	var swLat = map.getBounds().getSouthWest().getLat(),
-		swLng = map.getBounds().getSouthWest().getLng(),
-		neLat = map.getBounds().getNorthEast().getLat(),
-		neLng = map.getBounds().getNorthEast().getLng();
-	let url = 'http://localhost:8080/HappyHome/main?action=map/rest/list&lngFrom=' + swLng + '&lngTo=' + neLng + '&latFrom=' + swLat + '&latTo=' + neLat;
-	fetch(url)
-		.then((res) => res.json())
-		.then((json) => {
-			makeList(json);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  var swLat = map.getBounds().getSouthWest().getLat(),
+    swLng = map.getBounds().getSouthWest().getLng(),
+    neLat = map.getBounds().getNorthEast().getLat(),
+    neLng = map.getBounds().getNorthEast().getLng();
+  let url =
+    'http://localhost:8080/HappyHome/main?action=map/rest/list&lngFrom=' +
+    swLng +
+    '&lngTo=' +
+    neLng +
+    '&latFrom=' +
+    swLat +
+    '&latTo=' +
+    neLat;
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      makeList(json);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-
 function readTextFile(file) {
-	var rawFile = new XMLHttpRequest();
-	rawFile.open('GET', file, false);
-	rawFile.onreadystatechange = function() {
-		if (rawFile.readyState === 4) {
-			if (rawFile.status === 200 || rawFile.status == 0) {
-				var allText = rawFile.responseText;
+  var rawFile = new XMLHttpRequest();
+  rawFile.open('GET', file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
 
-				alert(allText);
-			}
-		}
-	};
-	rawFile.send(null);
+        alert(allText);
+      }
+    }
+  };
+  rawFile.send(null);
 }
 
 var isMoving = false;
-kakao.maps.event.addListener(map, 'drag', function() {
-	isMoving = true;
+kakao.maps.event.addListener(map, 'drag', function () {
+  isMoving = true;
 });
 
-kakao.maps.event.addListener(map, 'dragend', function() {
-	isMoving = false;
-	drawApts();
+kakao.maps.event.addListener(map, 'dragend', function () {
+  isMoving = false;
+  drawApts();
 });
 
 // from live
 
 function initTable(showDate) {
-	let tbody = document.querySelector('#aptlist');
-	let len = tbody.rows.length;
-	for (let i = len - 1; i >= 0; i--) {
-		tbody.deleteRow(i);
-	}
+  let tbody = document.querySelector('#aptlist');
+  let len = tbody.rows.length;
+  for (let i = len - 1; i >= 0; i--) {
+    tbody.deleteRow(i);
+  }
 
-	// 아파트 리스트의 header를 변경합니다.
-	let headerRow = document.querySelector('#aptHeader');
-	if (headerRow) {
-		// 기존 header를 삭제합니다.
-		headerRow.innerHTML = '';
+  // 아파트 리스트의 header를 변경합니다.
+  let headerRow = document.querySelector('#aptHeader');
+  if (headerRow) {
+    // 기존 header를 삭제합니다.
+    headerRow.innerHTML = '';
 
-		// 새로운 header를 생성하여 추가합니다.
-		let aptHeader = ['아파트이름', '층', '면적', '거래금액'];
-		if (showDate) {
-			aptHeader.push('거래날짜');
-		}
+    // 새로운 header를 생성하여 추가합니다.
+    let aptHeader = ['아파트이름', '층', '면적', '거래금액'];
+    if (showDate) {
+      aptHeader.push('거래날짜');
+    }
 
-		aptHeader.forEach((item) => {
-			let th = document.createElement('th');
-			th.appendChild(document.createTextNode(item));
-			headerRow.appendChild(th);
-		});
-	}
+    aptHeader.forEach((item) => {
+      let th = document.createElement('th');
+      th.appendChild(document.createTextNode(item));
+      headerRow.appendChild(th);
+    });
+  }
 }
 
 function viewMap(aptName, aptCode, lat, lng) {
-	if (marker != null && infowindow != null) {
-		marker.setMap(null);
-		infowindow.close();
-	}
-	
-	// 주소-좌표 변환 객체를 생성합니다
-	let url = "http://localhost:8080/HappyHome/main?action=map/rest/aptList&aptCode=" + aptCode;
-	let tbody = document.querySelector('#aptlist');
-	fetch(url)
-		.then((res) => res.json())
-		.then((jsons) => {
-			initTable(true);
-			jsons.forEach((json) => {
-				let tr = document.createElement('tr');
+  if (marker != null && infowindow != null) {
+    marker.setMap(null);
+    infowindow.close();
+  }
 
-				let nameTd = document.createElement('td');
-				nameTd.appendChild(document.createTextNode(aptName));
-				tr.appendChild(nameTd);
+  // 주소-좌표 변환 객체를 생성합니다
+  let url = 'http://localhost:8080/HappyHome/main?action=map/rest/aptList&aptCode=' + aptCode;
+  let tbody = document.querySelector('#aptlist');
+  fetch(url)
+    .then((res) => res.json())
+    .then((jsons) => {
+      initTable(true);
+      jsons.forEach((json) => {
+        let tr = document.createElement('tr');
 
-				let floorTd = document.createElement('td');
-				floorTd.appendChild(document.createTextNode(json.floor));
-				tr.appendChild(floorTd);
+        let nameTd = document.createElement('td');
+        nameTd.appendChild(document.createTextNode(aptName));
+        tr.appendChild(nameTd);
 
-				let areaTd = document.createElement('td');
-				areaTd.appendChild(document.createTextNode(json.area));
-				tr.appendChild(areaTd);
+        let floorTd = document.createElement('td');
+        floorTd.appendChild(document.createTextNode(json.floor));
+        tr.appendChild(floorTd);
 
-				let priceTd = document.createElement('td');
-				priceTd.appendChild(document.createTextNode(json.dealAmount + '만원'));
-				priceTd.classList.add('text-end');
-				tr.appendChild(priceTd);
+        let areaTd = document.createElement('td');
+        areaTd.appendChild(document.createTextNode(json.area));
+        tr.appendChild(areaTd);
 
-				let dateTd = document.createElement('td');
-				dateTd.appendChild(document.createTextNode(json.dealYear + '/' + json.dealMonth + '/' + json.dealDay));
-				dateTd.classList.add('text-end');
-				tr.appendChild(dateTd);
+        let priceTd = document.createElement('td');
+        priceTd.appendChild(document.createTextNode(json.dealAmount + '만원'));
+        priceTd.classList.add('text-end');
+        tr.appendChild(priceTd);
 
+        let dateTd = document.createElement('td');
+        dateTd.appendChild(
+          document.createTextNode(json.dealYear + '/' + json.dealMonth + '/' + json.dealDay)
+        );
+        dateTd.classList.add('text-end');
+        tr.appendChild(dateTd);
 
-				tbody.appendChild(tr);
-			});
-		});
+        tbody.appendChild(tr);
+      });
+    });
 }
+
 function makeList(jsons) {
-	document.querySelector('table').setAttribute('style', 'display: ;');
-	let tbody = document.querySelector('#aptlist');
-	initTable(false);
-	if (marker != null && infowindow != null) {
-    	marker.setMap(null);
-    	infowindow.close();
-  	}
-  
-	jsons.forEach((json) => {
-		let tr = document.createElement('tr');
+  document.querySelector('table').setAttribute('style', 'display: ;');
+  let tbody = document.querySelector('#aptlist');
+  initTable(false);
+  if (marker != null && infowindow != null) {
+    marker.setMap(null);
+    infowindow.close();
+  }
 
-		let nameTd = document.createElement('td');
-		nameTd.appendChild(document.createTextNode(json.apartmentName));
-		tr.appendChild(nameTd);
+  jsons.forEach((json) => {
+    let tr = document.createElement('tr');
 
-		let floorTd = document.createElement('td');
-		floorTd.appendChild(document.createTextNode(json.floor));
-		tr.appendChild(floorTd);
+    let nameTd = document.createElement('td');
+    nameTd.appendChild(document.createTextNode(json.apartmentName));
+    tr.appendChild(nameTd);
 
-		let areaTd = document.createElement('td');
-		areaTd.appendChild(document.createTextNode(json.area));
-		tr.appendChild(areaTd);
+    let floorTd = document.createElement('td');
+    floorTd.appendChild(document.createTextNode(json.floor));
+    tr.appendChild(floorTd);
 
-		let priceTd = document.createElement('td');
-		priceTd.appendChild(document.createTextNode(json.dealAmount + '만원'));
-		priceTd.classList.add('text-end');
-		tr.appendChild(priceTd);
+    let areaTd = document.createElement('td');
+    areaTd.appendChild(document.createTextNode(json.area));
+    tr.appendChild(areaTd);
 
-		tr.addEventListener('click', () => viewMap(json.apartmentName, json.aptCode, json.lat, json.lng));
-		drawOverlay(json.lng, json.lat, json.apartmentName);
-		tbody.appendChild(tr);
-	});
+    let priceTd = document.createElement('td');
+    priceTd.appendChild(document.createTextNode(json.dealAmount + '만원'));
+    priceTd.classList.add('text-end');
+    tr.appendChild(priceTd);
+
+    tr.addEventListener('click', () =>
+      viewMap(json.apartmentName, json.aptCode, json.lat, json.lng)
+    );
+    drawOverlay(json.lng, json.lat, json.apartmentName);
+    tbody.appendChild(tr);
+  });
 }
 
 function drawOverlay(lng, lat, apartmentName) {
-   var coords = new kakao.maps.LatLng(lat, lng);
+  var coords = new kakao.maps.LatLng(lat, lng);
 
   // 결과값으로 받은 위치를 마커로 표시합니다
   marker = new kakao.maps.Marker({
     map: map,
     position: coords,
   });
-console.log(lat, lng, apartmentName);
+  console.log(lat, lng, apartmentName);
   // 인포윈도우로 장소에 대한 설명을 표시합니다
   infowindow = new kakao.maps.InfoWindow({
     content: `<div style="width:150px;text-align:center;padding:6px 0;">${apartmentName}</div>`,
