576fd1c1adbecce8a1b897af9d50021c6d6cc81e
Hwarang <keyein123@gmail.com>
2024-05-20 06:18:03 +0900

    240520_Morning_Check_Hwarang

--- Alg_TempSolving/src/temp.java
+++ Alg_TempSolving/src/temp.java
@@ -2,8 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class temp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        String target = input.readLine();
+        int idx = Integer.parseInt(input.readLine());
+        System.out.println(target.charAt(idx - 1));
     }
 }
