f046838101ad9edc438ec206946c54489ee254d1
Hwarang <keyein123@gmail.com>
2024-06-16 23:13:44 +0900

    240616_Alg Solve

--- Alg_TempSolving/src/Main2.java
+++ Alg_TempSolving/src/Main2.java
@@ -2,57 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main2 {
+    static class PossRun {
+        long start, end;
+
+        public PossRun(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public String toString() {
+            return "[ Start : " + this.start + " End : " + this.end + " ]";
+        }
+    }
+
+    static class Solution {
+        int N;
+        long T;
+        ArrayList<PossRun> runningList;
+
+        void run() throws IOException {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st1 = new StringTokenizer(input.readLine());
+            StringTokenizer st2 = null;
+            this.N = Integer.parseInt(st1.nextToken());
+            this.T = (long) (Double.parseDouble(st1.nextToken()) * 10000);
+            this.runningList = new ArrayList<>(this.N);
+
+            st1 = new StringTokenizer(input.readLine());
+            st2 = new StringTokenizer(input.readLine());
+            long location, speed;
+            long start, end;
+            for (int i = 0; i < this.N; i++) {
+                location = Long.parseLong(st1.nextToken());
+                speed = Long.parseLong(st2.nextToken());
+
+                long delta = speed * this.T;
+
+                if (speed > 0 && this.T > 0 && delta / speed != this.T) {
+                    // Handle overflow by setting to max/min values
+                    start = Long.MIN_VALUE;
+                    end = Long.MAX_VALUE;
+                } else {
+                    start = location * 10000 - delta;
+                    end = location * 10000 + delta;
+                }
+
+                start = start < 1 ? 1 : start;
+                end = end > 1_000_000_000L * 10000 ? 1_000_000_000L * 10000 : end;
+                runningList.add(new PossRun(start, end));
+            }
+            System.out.println(checkMake());
+        }
+
+        int checkMake() {
+            long totalStart = 1;
+            long totalEnd = 1_000_000_000L * 10000;
+            for (PossRun each : runningList) {
+                if (each.start > totalStart)
+                    totalStart = each.start;
+                if (each.end < totalEnd)
+                    totalEnd = each.end;
+                if (totalStart > totalEnd) {
+                    return 0;
+                }
+            }
+            System.out.println(totalStart / 10000.0);
+            return 1;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String name = input.readLine();
-        boolean[] notAarr = new boolean[name.length()]; // A가 아닌 친구들 -> 정신개조 필요한 애들
-        int[] distArr = new int[name.length()]; // dist ARR -> 실시간으로 바꿔줘야 함 ㅇㅇ
-
-        int totalCost = 0; // 말그대로..
-        int notA = 0; // Not A인 친구들 개수 -> 이만큼 정신개조 돌 예정
-        for (int i = 0; i < name.length(); i++) {
-            totalCost += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1); // 다 똑같을듯
-            if (name.charAt(i) != 'A') {
-                notA++;
-                notAarr[i] = true;
-            }
-            distArr[i] = Math.min(i, Math.abs(i - name.length())); // 시작점 0번째 인덱스 // 0 1 2 3 4 3 2 1 // D B A A A B
-        }
-
-        int minIndex;
-
-        for (int i = 0; i < notA; i++) {
-            minIndex = getMinCostIndex(distArr, notAarr);
-            totalCost += distArr[minIndex];
-            notAarr[minIndex] = false;
-            if (minIndex == 0) {
-
-            }
-            calculateDist(distArr, minIndex);
-        }
-        System.out.println(totalCost);
+        Solution solution = new Solution();
+        solution.run();
     }
-
-    public static int getMinCostIndex(int[] distArr, boolean[] notAarr) {
-        int minIndex = 0;
-        int minCost = Integer.MAX_VALUE;
-        for (int i = 0; i < distArr.length; i++) {
-            if (!notAarr[i])
-                continue;
-            if (distArr[i] < minCost) {
-                minCost = distArr[i];
-                minIndex = i; //
-            }
-
-        }
-        return minIndex;
-    }
-
-    public static void calculateDist(int[] distArr, int minIndex) {
-        for (int i = 0; i < distArr.length; i++) {
-            distArr[i] = Math.min(Math.abs(minIndex - i), Math.abs(Math.abs(minIndex - i) - distArr.length));
-        }
-    }
-
 }
