1362fa02aa3f4cd4b2327043463258de9253a484
Hwarang <keyein123@gmail.com>
2024-06-19 23:53:22 +0900

    240619_Alg Solving

--- Alg_TempSolving/src/Main.java
+++ Alg_TempSolving/src/Main.java
@@ -1,35 +1,10 @@
 import java.io.*;
-import java.util.*;
+import java.time.LocalDate;
 
 public class Main {
     static class Solution {
-        boolean Up, Down, Mixed;
-
         void run() throws IOException {
-            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(input.readLine());
-            int past, current;
-            past = current = Integer.parseInt(st.nextToken());
-            while (st.hasMoreTokens()) {
-                current = Integer.parseInt(st.nextToken());
-                if (current > past)
-                    Up = true;
-                else
-                    Down = true;
-                if (Up && Down) {
-                    Mixed = true;
-                    break;
-                }
-                past = current;
-            }
-            if (Mixed) {
-                System.out.println("mixed");
-            } else {
-                if (Up)
-                    System.out.println("ascending");
-                else
-                    System.out.println("descending");
-            }
+            System.out.println(LocalDate.now());
         }
     }
 
