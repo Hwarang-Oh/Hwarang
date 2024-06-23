afc97e50caf2689930750162f336e5a2cb8cb030
Hwarang <keyein123@gmail.com>
2024-06-23 18:18:27 +0900

    240623 Alg Solve

--- Alg_TempSolving/src/Main.java
+++ Alg_TempSolving/src/Main.java
@@ -1,15 +1,3 @@
-import java.io.*;
-import java.time.LocalDate;
-
 public class Main {
-    static class Solution {
-        void run() throws IOException {
-            System.out.println(LocalDate.now());
-        }
-    }
 
-    public static void main(String[] args) throws IOException {
-        Solution solution = new Solution();
-        solution.run();
-    }
 }
