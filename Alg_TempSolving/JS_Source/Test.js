ca17560d6818533401620606d53c9c23ff27ad3a
Hwarang <keyein123@gmail.com>
2024-09-17 17:09:31 +0900

    ✨ feat : Algorithm Solved - ✅ : Algorithm Solved In Programmers - ✅ : Organized Solved File to ALG_Solve Folder - ✅ : Made Test.js File with Cheat Sheet of JS Algorithm Solving Tool

--- Alg_TempSolving/JS_Source/Test.js
+++ Alg_TempSolving/JS_Source/Test.js
@@ -1,7 +1,84 @@
-const [N, S] = require('fs')
+/**
+ * IMP : get Input in JS : readFileSync() ( 1 )
+ */
+const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
   .split('\n')
   .map(Number);
-console.log(N * S);
+
+/**
+ * IMP : get Input in JS : readline() ( 2 )
+ */
+const readline = require('readline');
+const br = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n = 0;
+let lineCount = -1;
+br.on('line', (line) => {
+  if (lineCount === -1) {
+    lineCount = parseInt(line);
+    n = lineCount;
+    return;
+  }
+  line.split(' ').forEach((value) => {});
+
+  lineCount--;
+  if (lineCount === 0) br.close();
+}).on('close', () => {
+  console.log(pq.peek());
+  process.exit();
+});
+
+/**
+ * IMP : 입력 다루는 방식
+ */
+let [N, M] = input[0].split(' ').map(Number); // N, M 값 받기
+let targetArray = input.slice(1).map((line) => line.trim().split('').map(Number)); // 다음 줄에 나오는 target Array 받기 ( shift, slice 모두 가능 )
+let map = Array.from({ length: M }, () => Array(N).fill(0));
+let visited = Array.from({ length: M }, () => Array(N).fill(-1)); // 2차원 배열 쉽게 선언하기
+const move = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+]; // move Array 만들기
+
+/**
+ * IMP : Class 선언하기
+ */
+class Pair {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/**
+ * IMP : PQ 간단하게 구현
+ */
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+  offer(value) {
+    this.items.push(value);
+    this.items.sort((a, b) => a - b);
+  }
+  poll() {
+    return this.items.shift();
+  }
+  peek() {
+    return this.items[0];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+}
