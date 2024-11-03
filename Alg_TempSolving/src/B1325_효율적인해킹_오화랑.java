793d3191c404f5f346e2a71e88f13b5a51d92b2b
Hwarang <keyein123@gmail.com>
2024-11-03 22:49:35 +0900

    ✨ Solved : B1325, B1600, B13905, B15486, B15990, B16169

--- Alg_TempSolving/src/B1325_효율적인해킹_오화랑.java
+++ Alg_TempSolving/src/B1325_효율적인해킹_오화랑.java
@@ -15,67 +15,67 @@ import java.util.*;
  * => 1 : ()
  * => 일단 내가 알던 위상 정렬 느낌이 아니긴 함 => 약간 Set으로 나를 믿는 사람들과 내가 믿는 사람들을 관리하는 느낌
  * => BFS로 하고 다시 그냥 최적해 구하는 것을 다시 해보자!!
+ * => 내가 갔던 경로는 기억하는 것이 좋을 것 같긴 한데... 이미 연결된 Graph에서 => 특정 친구를 Visited할 때 => 내가
+ * 그 장소를 더 많은 것을 들린 채로 왔는가? 판단이 중요하지 않을까?
  */
 
 public class B1325_효율적인해킹_오화랑 {
-    static class relationState {
-        HashSet<Integer> trustToList;
-        HashSet<Integer> trustFromList;
-
-        public relationState(int i) {
-            this.trustToList = new HashSet<>();
-            this.trustToList.add(i);
-            this.trustFromList = new HashSet<>();
-        }
-
-        public String toString() {
-            return "trustToList : " + trustToList.toString() + ", trustFromList : " + trustFromList.toString();
-        }
-    }
 
     static class Solution {
-        int N, M, MAX;
-        ArrayList<Integer> resultList = new ArrayList<>();
-        ArrayList<relationState> network = new ArrayList<>();
+        int N, M, MAXHACK;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        int[] howMuch, result;
+        boolean[] visited;
 
         void run() throws IOException {
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(input.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-            for (int i = 0; i <= N; i++)
-                network.add(new relationState(i));
+            howMuch = new int[N + 1];
+            result = new int[N + 1];
+            visited = new boolean[N + 1];
 
-            int from, to, currentSize;
+            for (int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
+            }
+
+            int from, to;
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(input.readLine());
                 from = Integer.parseInt(st.nextToken());
                 to = Integer.parseInt(st.nextToken());
-                if (!network.get(to).trustFromList.contains(from)) {
-                    // 3 -> 1 ( 3이 1을 신뢰한다 )
-                    // x - > 3 ( x가 3을 신뢰한다 => x -> 1을 신뢰하는 셈 )
-                    network.get(to).trustFromList.addAll(network.get(from).trustFromList);
-                }
+                graph.get(to).add(from);
             }
 
-            for (relationState each : network)
-                System.out.println(each);
-
-            // int from, to;
-            // for (int i = 0; i < M; i++) {
-            // st = new StringTokenizer(input.readLine());
-            // from = Integer.parseInt(st.nextToken());
-            // to = Integer.parseInt(st.nextToken());
-            // if (!network.get(to).bList.contains(from)) {
-            // network.get(to).bList.add(from);
-            // network.get(from).canHackNum += network.get
-            // }
-            // }
+            for (int i = 1; i <= N; i++) {
+                if (visited[i])
+                    continue;
+                int count = 0;
+                queue.offer(i);
+                visited[i] = true;
+                while (!queue.isEmpty()) {
+                    int current = queue.poll();
+                    count++;
+                    for (int next : graph.get(current)) {
+                        if (visited[next] && howMuch[next] > howMuch[current])
+                            continue;
+                        queue.offer(next);
+                        howMuch[next] = howMuch[current] + 1;
+                        visited[next] = true;
+                    }
+                }
+                result[i] = count;
+            }
+            for (int i = 0; i <= N; i++) {
+                System.out.println("i : " + i + " , result :" + result[i]);
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
-        Solution Solution = new Solution();
-        Solution.run();
+        Solution solution = new Solution();
+        solution.run();
     }
 }
